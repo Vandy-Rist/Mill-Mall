739428dcabb2e94736daf598cc7381ce41a1e9d1
Vandy-Rist <lionfan1368@gmail.com>
2020-09-16 18:09:55 +0800

    ServiceBar

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -250,8 +250,9 @@
               overflow: hidden;
               border-top: 1px solid #E5E5E5;
               box-shadow: 0px 7px 6px 0px rgba(0, 0, 0, 0.11);
-              z-index: 10;
               transition: all .5s;
+              z-index: 10;
+              background-color: #fff;
               .product{
                 position: relative;
                 float: left;
