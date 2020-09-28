1cd6c4887440dbd0e1ef285dbd54e799e837d25f
Vandy-Rist <lionfan1368@gmail.com>
2020-09-28 16:22:16 +0800

    购物车列表、全选功能

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -6,22 +6,26 @@
        </div>
        <div class="title">
          <h2>{{title}}
-           <span><slot name="tip"></slot></span>
+           <slot name="tip"></slot>
          </h2>
          
        </div>
        <div class="username">
-         <a href="javascript:;">jack</a>
+         <a href="javascript:;">{{username}}</a>
        </div>
     </div>
   </div>
 </template> 
 <script>
+  import {mapState} from 'vuex'
   export default {
     name: 'order-header',
     props:{
       title:String
-    }
+    },
+    computed:{
+      ...mapState(['username'])
+    },
   }
 </script>
 
