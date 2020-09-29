de0b5695a089ecd526b6105413a90e2618e204ee
Vandy-Rist <lionfan1368@gmail.com>
2020-09-29 14:26:55 +0800

    购物车更新、删除、结算

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -21,7 +21,7 @@
           <ul class="cart-item-list">
             <li class="cart-item" v-for="(item,index) in list" :key="index">
               <div class="item-check">
-                <span class="checkbox" :class="{'checked':item.productSelected}"></span>
+                <span class="checkbox" :class="{'checked':item.productSelected}" @click="updateCart(item)"></span>
               </div>
               <div class="item-name">
                 <img v-lazy="item.productMainImage" alt="">
@@ -30,13 +30,13 @@
               <div class="item-price">{{item.productPrice}}</div>
               <div class="item-num">
                <div class="num-box">
-                  <a href="javascript:;">-</a>
+                  <a href="javascript:;" @click="updateCart(item,'-')">-</a>
                   <span>{{item.quantity}}</span>
-                  <a href="javascript:;">+</a>
+                  <a href="javascript:;" @click="updateCart(item,'+')">+</a>
                </div>
               </div>
               <div class="item-total">{{item.productTotalPrice}}</div>
-              <div class="item-del"></div>
+              <div class="item-del" @click="delProduct(item)"></div>
             </li>
           </ul>
         </div>
@@ -46,7 +46,7 @@
           </div>
           <div class="total fr">
             合计:<span>{{cartTotalPrice}}</span>元
-            <a href="javascript:;" class="btn">去结算</a>
+            <a href="javascript:;" class="btn" @click="order">去结算</a>
           </div>
         </div>
       </div>
@@ -84,8 +84,40 @@
           this.renderData(res);
         })
       },
+      // 更新购物车数量和购物车单选状态
+      updateCart(item,type){
+        let quantity = item.quantity,
+            selected = item.productSelected;
+        if(type == '-'){
+          if(quantity == 1){
+            alert('至少选择一件商品');
+            return;
+          }
+          --quantity;
+        }else if(type == '+'){
+          if(quantity > item.productStock){
+            alert('超过库存数量了哦');
+            return;
+          }
+          ++quantity;
+        }else{
+          selected = !item.productSelected;
+        }
+        this.axios.put(`/carts/${item.productId}`,{
+          quantity,
+          selected
+        }).then((res)=>{
+          this.renderData(res);
+        });
+      },
+      // 删除购物车商品
+      delProduct(item){
+        this.axios.delete(`/carts/${item.productId}`).then((res)=>{
+          this.renderData(res);
+        });
+      },
       toggleAll(){
-        let url = this.allchecked ? '/carts/unSelectAll': '/carts/selectAll';
+        let url = this.allchecked ? '/carts/unSelectAll' : '/carts/selectAll';
         this.axios.put(url).then((res)=>{
           this.renderData(res);
         })
@@ -95,8 +127,16 @@
         this.allchecked = res.selectedAll;
         this.cartTotalPrice = res.cartTotalPrice;
         this.checkedNum = this.list.filter(item=>item.productSelected).length;
+      },
+      // 购物车下单
+      order(){
+        let isCheck = this.list.every(item=>!item.productSelected);
+        if(isCheck){
+          alert('请选择一件商品');
+        }else{
+          this.$router.push('/order/confirm');
+        }
       }
-
     }
   }
 </script>
